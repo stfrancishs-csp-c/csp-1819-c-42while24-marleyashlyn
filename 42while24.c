460c91c271740e65adaad9becfb471da6304aa0f
Marley Foote <19foote@stfrancishs.org>
2018-10-01 19:39:25 +0000

    update

--- 42while24.c
+++ 42while24.c
@@ -2,21 +2,19 @@
 int main(void)
 {
     int sum=0;
-    int k;
+    int k=0;
 
-    for (k=0;k<5;k++)
+    while (k<5)
     {
-        sum+=k; //use something different in "while" version
+        sum=sum+k;
+        k++;
     }
-
+    printf ("sum =");
     printf ("%d\n",sum);
 
-    k=5;
+        for (k=9;k>0;k--)
+     {
 
-    while (k>0) {
-        k=k-1; //use something different in "for" version
-        printf ("%d\n", k);
-    }
-
+    printf ("%d\n", k);
+     }
 }
-
